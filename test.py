85934bcee2a334978fead7d980c955b89554ce02
Kent Slaney <kentslaney@users.noreply.github.com>
2024-07-22 12:06:16 -0700

    test and patch for language detection via exponential backoff (#2)

--- test.py
+++ test.py
@@ -60,14 +60,30 @@ class Test(AudioFileStitch):
 
 class MockTokenizer:
     def __init__(self, language, **kw):
-        self.language = language
+        self.language, self._kw = language, kw
         for k, v in kw.items():
             setattr(self, k, v)
 
-    def encode(prompt):
+    def encode(self, prompt):
         return [self.language, self, prompt]
 
+class OnDemand:
+    def __init__(self, seq=(), relative=True):
+        self.seq, self.relative = seq, relative
+        self.prev, self.given = 0, 0
+
+    def __getitem__(self, key):
+        _key = self.given if self.relative else key
+        self.prev = self.seq[_key] if _key < len(self.seq) else int(
+                input(f"lang @ {_key}: ") or self.prev)
+        self.given += 1
+        return self.prev
+
+    def __len__(self):
+        return CHUNK_LENGTH + 1 if self.relative else len(self.seq)
+
 class TranscriberTest(Transcriber):
+    sample = object()
     dtype = torch.float32
     model = type("MockModel", (), {
             "is_multilingual": True,
@@ -76,22 +92,39 @@ class TranscriberTest(Transcriber):
         })()
     _seek = 0
 
-    def __init__(self, seq):
-        super().__init__(self.model)
-        self.seq = seq
+    def __init__(self, seq=None):
+        super().__init__(self.model, initial_prompt="")
+        self.seq = OnDemand(seq or ())
         self.result = []
-        self.result.append(self.initial_prompt_tokens)
-        self.latest = []
+        self.latest = np.empty((0,))
         for i in range(len(self.seq)):
             self._seek = i
-            self.result.append(self.initial_prompt_tokens)
+            self.frame_offset = max(0, i + 1 - CHUNK_LENGTH)
+            res = self.initial_prompt_tokens
+            assert res[0] == self.seq.prev
+            self.result.append(res[1:])
+            if seq is None:
+                print(res)
 
-    def detect_language(self):
-        self.result.append("sample")
+    def detect_language(self, mel=None):
+        self.result.append([self.sample, mel])
         return self.seq[self._seek]
 
-    def get_tokenizer(self, language, **kw):
-        return MockTokenizer(language, **kw)
+    def get_tokenizer(self, multilingual, language, **kw):
+        return MockTokenizer(language, **{"multilingual": multilingual, **kw})
+
+    @property
+    def rle(self):
+        res = []
+        for i, *j in self.result:
+            if i is self.sample:
+                res.append(0)
+            else:
+                res[-1] += 1
+        return res
+
+if __name__ == "__main__":
+    print(TranscriberTest([0, 0, 1, 0, 0, 0, 0, 0, 0]).rle)
 
 class ReadableMinimal(MinimalTranscriber, AudioTranscriber):
     def gutter(self, segment):
@@ -179,10 +212,9 @@ def mel_test(seq=test_files, check_amp=False):
 class ReadableProgress(ProgressTranscriber, ReadableMinimal):
     pass
 
-if __name__ == "__main__":
-    from whisper import load_model
-    import sys
-    transcriber = ReadableProgress(load_model("base.en"), verbose=False)
-    asyncio.run(transcriber.progressive(AudioFile(fname=sys.argv[1])))
-    print(transcriber)
-
+# if __name__ == "__main__":
+#     from whisper import load_model
+#     import sys
+#     transcriber = ReadableProgress(load_model("base.en"), verbose=False)
+#     asyncio.run(transcriber.progressive(AudioFile(fname=sys.argv[1])))
+#     print(transcriber)
